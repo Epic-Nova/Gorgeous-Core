5d3826d7a274084f66f13dbf97942df0e9172465
Nils Bergemann <nils.bergemann@icloud.com>
2026-04-24 03:12:53 +0200

    *   **Interaction Foundation**: Established core interfaces and structures for modular actor focus logic, decoupling debug visuals from functional interaction milestones. *   **State-Aware Diagnostics**: Implemented diamond-based state resolution with precise coordinate boundary checks to visualize interaction flow points. *   **System Integrity**:     *   Introduced the **Gorgeous General System Validator** for proactive startup diagnostics.     *   Enhanced **Plugin Resource Guard** for strict dependency validation.     *   Improved **Setup Wizard UI** and stabilized the **Insight Matrix** provider.

---
+++ Source/GorgeousCoreRuntime/Public/GeneralSystems/InteractionFoundation/GorgeousInteractionFoundationBlueprintFunctionLibrary.h
@@ -0,0 +1,126 @@
+// Copyright (c) 2026 Simsalabim Studios (Nils Bergemann). All rights reserved.
+/*==========================================================================>
+|               Gorgeous Core - Core functionality provider                 |
+| ------------------------------------------------------------------------- |
+|         Copyright (C) 2026 Gorgeous Things by Simsalabim Studios,         |
+|              administrated by Epic Nova. All rights reserved.             |
+| ------------------------------------------------------------------------- |
+|                    Epic Nova is an independent entity,                    |
+|        that has nothing in common with Epic Games in any capacity.        |
+<==========================================================================*/
+#pragma once
+
+//<=============================--- Includes ---=============================>
+//<--------------------------=== Module Includes ===------------------------->
+#include "GeneralSystems/InteractionFoundation/InteractionFoundation_I.h"
+#include "GeneralSystems/InteractionFoundation/InteractionFoundationStructures.h"
+#include "GeneralSystems/DebugAssist/DebugAssistStructures.h"
+#include "GameplayTagContainer.h"
+//----------------=== Third Party & Miscellaneous Includes ===--------------->
+#include "GorgeousInteractionFoundationBlueprintFunctionLibrary.generated.h"
+//<-------------------------------------------------------------------------->
+
+/*
+<=============================--- Class Info ---============================>
+<-----------------------------=== Quick Info ===---------------------------->
+| Display Name: Gorgeous Interaction Foundation Blueprint Function Library
+| Functional Name: UGorgeousInteractionFoundationBlueprintFunctionLibrary
+| Parent Class: UBlueprintFunctionLibrary
+| Class Suffix: -
+| Author: Nils Bergemann
+<--------------------------------------------------------------------------->
+<--------------------------=== Class Description ===------------------------>
+| Function Library providing helper functions for 
+| interaction related functionality in Blueprints.
+<--------------------------------------------------------------------------->
+<===========================================================================>
+*/
+UCLASS()
+class GORGEOUSCORERUNTIME_API UGorgeousInteractionFoundationBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+
+    /**
+     * Tries to request the interaction tags from the target actor. Returns false if the target actor does not implement the interaction interface or if the request failed for any reason.
+     * 
+     * @param TargetActor The actor to request the interaction tags from.
+     * @param OutInteractionTags The output parameter that will contain the requested interaction tags if the function returns true.
+     * 
+     * @return True if the interaction tags were successfully requested, false otherwise.
+     */
+    UFUNCTION(BlueprintCallable, Category = "Gorgeous Core|Interaction Foundation")
+    static bool TryRequestInteractionTags(AActor* TargetActor, FGameplayTagContainer& OutInteractionTags);
+    
+    /**  
+     * Tries to check if interaction with the target actor is currently possible. Returns false if the target actor does not implement the interaction interface or if the check failed for any reason.
+     * 
+     * @param TargetActor The actor to check for interaction possibility.
+     * @param InteractingActor The actor that is trying to interact with the target actor. This can be used by the target actor to determine if interaction is possible based on the context of the interacting actor.
+     * @param bCanInteract The output parameter that will contain the result of the interaction possibility check if the function returns true.
+     * 
+     * @return True if the interaction possibility was successfully checked, false otherwise.
+     */
+    UFUNCTION(BlueprintCallable, Category = "Gorgeous Core|Interaction Foundation")
+    static bool TryCanInteract(AActor* TargetActor, AActor* InteractingActor, bool& bCanInteract);
+
+    /** 
+     * Tries to request the focus data from the target actor. Returns false if the target actor does not implement the interaction interface or if the request failed for any reason.
+     * 
+     * @param TargetActor The actor to request the focus data from.
+     * @param InteractingActor The actor that is trying to focus on the target actor. This can be used by the target actor to determine the focus data based on the context of the interacting actor.
+     * @param OutFocusData The output parameter that will contain the requested focus data if the function returns true.
+     * 
+     * @return True if the focus data was successfully requested, false otherwise.
+     */
+    UFUNCTION(BlueprintCallable, Category = "Gorgeous Core|Interaction Foundation")
+    static bool TryFocus(AActor* TargetActor, AActor* InteractingActor, FInstancedStruct& OutFocusData);
+
+    /** 
+     * Tries to execute the interaction with the target actor. Returns false if the target actor does not implement the interaction interface or if the interaction failed for any reason.
+     * 
+     * @param TargetActor The actor to interact with.
+     * @param InteractingActor The actor that is trying to interact with the target actor. This can be used by the target actor to determine the context of the interaction and to apply effects or changes to the interacting actor if necessary.
+     * 
+     * @return True if the interaction was successfully executed, false otherwise.
+     */
+    UFUNCTION(BlueprintCallable, Category = "Gorgeous Core|Interaction Foundation")
+    static bool TryInteract(AActor* TargetActor, AActor* InteractingActor);
+
+    /** 
+     * Tries to perform a sphere trace interaction based on the provided trace parameters and interaction tag. Returns false if the trace did not hit any valid interactable target or if the interaction failed for any reason.
+     * 
+     * @param WorldContextObject The world context object for locating the world in which to perform the trace.
+     * @param TraceParameters The parameters defining the sphere trace, such as start and end locations, radius, and collision channel.
+     * @param InteractionTag The gameplay tag representing the type of interaction to perform. This can be used by the hit actor to determine if it supports this type of interaction and how to handle it.
+     * @param OutHitResult The output parameter that will contain the hit result of the trace if the function returns true.
+     * 
+     * @return True if a valid interactable target was hit and the interaction was successfully executed, false otherwise.
+     */
+    UFUNCTION(BlueprintCallable, Category = "Gorgeous Core|Interaction Foundation", meta = (WorldContext = "WorldContextObject", CompactNodeTitle = "Try Interact (Sphere Trace)"))
+    static bool TrySphereTraceInteract(const UObject* WorldContextObject,
+        const FGorgeousInteractionSphereTraceParameters& TraceParameters,
+        FGameplayTag InteractionTag,
+        FHitResult& OutHitResult);
+
+    /** 
+     * Tries to perform a sphere trace focus based on the provided trace parameters and interaction tag. Returns false if the trace did not hit any valid interactable target or if the focus request failed for any reason.
+     * 
+     * @param WorldContextObject The world context object for locating the world in which to perform the trace.
+     * @param TraceParameters The parameters defining the sphere trace, such as start and end locations, radius, and collision channel.
+     * @param InteractionTag The gameplay tag representing the type of interaction to focus on. This can be used by the hit actor to determine if it supports this type of interaction and how to provide the focus data for it.
+     * @param OutFocusData The output parameter that will contain the focus data provided by the hit actor if the function returns true.
+     * @param OutHitResult The output parameter that will contain the hit result of the trace if the function returns true.
+     * 
+     * @return True if a valid interactable target was hit and the focus data was successfully requested, false otherwise.
+     */
+    UFUNCTION(BlueprintCallable, Category = "Gorgeous Core|Interaction Foundation", meta = (WorldContext = "WorldContextObject", CompactNodeTitle = "Try Focus (Sphere Trace)"))
+    static bool TrySphereTraceFocus(const UObject* WorldContextObject,
+        const FGorgeousInteractionSphereTraceParameters& TraceParameters,
+        FGameplayTag InteractionTag,
+        FInstancedStruct& OutFocusData,
+        FHitResult& OutHitResult);
+};
+
+using UGT_InteractionFoundation_FL = UGorgeousInteractionFoundationBlueprintFunctionLibrary;
---
+++ Source/GorgeousCoreRuntime/Public/GeneralSystems/InteractionFoundation/InteractionFoundationStructures.h
@@ -0,0 +1,72 @@
+// Copyright (c) 2026 Simsalabim Studios (Nils Bergemann). All rights reserved.
+/*==========================================================================>
+|               Gorgeous Core - Core functionality provider                 |
+| ------------------------------------------------------------------------- |
+|         Copyright (C) 2026 Gorgeous Things by Simsalabim Studios,         |
+|              administrated by Epic Nova. All rights reserved.             |
+| ------------------------------------------------------------------------- |
+|                    Epic Nova is an independent entity,                    |
+|        that has nothing in common with Epic Games in any capacity.        |
+<==========================================================================*/
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GorgeousCoreMinimalShared.h"
+#include "GeneralSystems/DebugAssist/DebugAssistStructures.h"
+#include "InteractionFoundationStructures.generated.h"
+
+USTRUCT(BlueprintType)
+struct GORGEOUSCORERUNTIME_API FGorgeousInteractionSphereTraceParameters
+{
+    GENERATED_BODY()
+
+    FGorgeousInteractionSphereTraceParameters()
+        : Start(FVector::ZeroVector)
+        , End(FVector::ZeroVector)
+        , Radius(100.0f)
+        , TraceChannel(ECC_WorldDynamic)
+        , bTraceComplex(false)
+        , bIgnoreSelf(true)
+        , ActorsToIgnore()
+        , DebugVisualParameters()
+    {
+        DebugVisualParameters.bEnabled = true;
+        DebugVisualParameters.bDrawSweptSphere = true;
+        DebugVisualParameters.bWireframe = false;
+        DebugVisualParameters.TracePathColor = FColor::White;
+        DebugVisualParameters.SweptSphereHitColor = FColor(0, 255, 0, 32);
+        DebugVisualParameters.SweptSphereMissColor = FColor(255, 0, 0, 32);
+        DebugVisualParameters.SweptSphereSteps = 10;
+        DebugVisualParameters.SweptSphereSegments = 16;
+        
+        DebugVisualParameters.HitSphere.bDraw = false;
+        
+        DebugVisualParameters.HitBounds.bDraw = true;
+        DebugVisualParameters.HitBounds.bDrawFilled = true;
+        DebugVisualParameters.HitBounds.bPulse = true;
+    }
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    FVector Start = FVector::ZeroVector;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    FVector End = FVector::ZeroVector;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    float Radius = 15.0f;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    TEnumAsByte<ECollisionChannel> TraceChannel = ECC_WorldDynamic;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    bool bTraceComplex = false;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    bool bIgnoreSelf = true;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    TArray<AActor*> ActorsToIgnore;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Interaction Foundation")
+    FGorgeousDebugAssistVisualParameters DebugVisualParameters;
+};
